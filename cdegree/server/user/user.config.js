7131e113e01244bad2192f4da75e37f13a8c3572
sebastian <sebastian.ruiz.va@gmail.com>
2018-10-30 12:08:22 -0400

    courses added to db

--- cdegree/server/user/user.config.js
+++ cdegree/server/user/user.config.js
@@ -2,11 +2,9 @@ Accounts.onCreateUser(function(options, user) {
 
     user.profile = {
         username: options.profile.username,
-        role: 'LISTENER',
         completedCourses:[]
     };
 
-    console.log(user);
     return user;
 
 });
