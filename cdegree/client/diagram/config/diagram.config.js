e18b65ebb80b571087a5d8d447a9730bcaff8c2f
sebastian <sebastian.ruiz.va@gmail.com>
2018-11-01 19:25:22 -0400

    tree improved

--- cdegree/client/diagram/config/diagram.config.js
+++ cdegree/client/diagram/config/diagram.config.js
@@ -2,4 +2,5 @@ Router.route('/diagram', function () {
     this.layout('layoutDefault');
     this.render('diagramSidebar', {to: 'sidebar'});
     this.render('diagram', {to: 'content'});
+    this.render('overlayCourse', {to: 'overlay'});
 });
