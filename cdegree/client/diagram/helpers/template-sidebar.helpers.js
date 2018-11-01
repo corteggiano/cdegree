81e5fcbcafa952b72efa3b67ff7ce5ca2f8271d9
Marie Yatsyk <maryats@users.noreply.github.com>
2018-11-01 19:48:30 -0400

    Add electives to sidebar

--- cdegree/client/diagram/helpers/template-sidebar.helpers.js
+++ cdegree/client/diagram/helpers/template-sidebar.helpers.js
@@ -1,15 +1,15 @@
 Template.diagramSidebar.helpers({
 
 // returns whether the current course has been completed or not
-  degree:function(){
+  degree: function () {
     let user = Meteor.user().profile.degree;
-    return Degree.findOne({name:user});
+    return Degree.findOne({name: user});
   },
 
   // returns incomplete courses
-  remaining:function(){
+  remaining: function () {
     let user = Meteor.user().profile;
-    let degree = Degree.findOne({name:user.degree});
+    let degree = Degree.findOne({name: user.degree});
 
     let degreeReqs = degree.sections.majorRequirements.slice();
     let userCompletedClasses = user.completedReqCourses;
@@ -17,7 +17,7 @@ Template.diagramSidebar.helpers({
     let remaining = [];
 
     degreeReqs.forEach((req) => {
-      if (!userCompletedClasses.includes(req)){
+      if (!userCompletedClasses.includes(req)) {
         let course = Course.findOne({id: req});
         remaining.push(course);
         console.log(course);
@@ -28,5 +28,31 @@ Template.diagramSidebar.helpers({
   },
 
   // returns incomplete electives
+  electives: function () {
+    let user = Meteor.user().profile;
+    let degree = Degree.findOne({name: user.degree});
 
+    let electives = degree.sections.electives.slice();
+    let userCompletedElectives = user.completedElectives.slice().map(
+        el => el[0]);
+
+    userCompletedElectives.forEach(el => {
+      // guard against student completing more electives than required
+      let idx = electives.indexOf(el);
+
+      if (electives.length > 0 && idx > -1) {
+        electives.splice(idx, 1); // remove completed elective
+      }
+    });
+
+    let descriptions = [];
+    // get the description for each elective
+
+    electives.forEach(el => {
+      let description = Elective.findOne({id: el}).name;
+      descriptions.push(description);
+    });
+
+    return descriptions;
+  },
 });
