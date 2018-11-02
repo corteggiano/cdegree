bd9b9a957eab31c2fe47391c04c1070886b5c7d7
Marie <marie.yatsyk@gmail.com>
2018-11-01 21:55:34 -0400

    users fixed (#13)

--- cdegree/client/diagram/events/sidebar.events.js
+++ cdegree/client/diagram/events/sidebar.events.js
@@ -2,7 +2,7 @@ Template.diagramSidebar.events({
 
   'click .classes': function(event) {
     $('#overlay').fadeIn();
-    Session.set('selectedCourse', event.currentTarget.dataset.id);
+      Session.set('selectedCourse', event.currentTarget.dataset.id);
   }
 
 });
