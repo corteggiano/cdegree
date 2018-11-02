f1f562c44cb83c32fac12ec91a1f220dcd2942f2
Zack Reiter <zack-r@users.noreply.github.com>
2018-11-01 22:19:45 -0400

    Sev (#15)
    * users fixed
    
    * users fixed

--- cdegree/client/diagram/events/overlay.events.js
+++ cdegree/client/diagram/events/overlay.events.js
@@ -2,6 +2,10 @@ Template.overlayCourse.events({
 
     'click .overlay-close': function(event) {
         $('#overlay').fadeOut();
+    },
+
+    'click .select-elective': function(event) {
+        Meteor.user().profile.selectedElectives.push([Session.get("selectedCurse"), event.currentTarget.dataset.id]);
     }
 
 });
