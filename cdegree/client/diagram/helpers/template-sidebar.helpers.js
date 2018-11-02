1981f4c8c0751dca307b71cecbf41c5498268598
Zack Reiter <zack-r@users.noreply.github.com>
2018-11-01 21:44:33 -0400

    Sidebar selected electives (#12)
    * Pull in classes in sidebar
    
    * Add electives to sidebar
    
    * Add numCredits calculations
    
    * selected  electives
    
    * selected elective stuff
    
    * selected elective stuff works

--- cdegree/client/diagram/helpers/template-sidebar.helpers.js
+++ cdegree/client/diagram/helpers/template-sidebar.helpers.js
@@ -21,6 +21,8 @@ function getRemainingElectiveIds() {
   let electives = Degree.findOne({name: user.degree}).sections.electives.slice();
   let userCompletedElectives = user.completedElectives.slice().map(
       el => el[0]);
+  let userSelectedElectives = user.selectedElectives.slice().map(
+      el => el[0]);
 
   userCompletedElectives.forEach(el => {
     // guard against student completing more electives than required
@@ -28,6 +30,15 @@ function getRemainingElectiveIds() {
 
     if (electives.length > 0 && idx > -1) {
       electives.splice(idx, 1); // remove completed elective
+    }
+  });
+
+  userSelectedElectives.forEach(el => {
+    // guard against student completing more electives than required
+    let idx = electives.indexOf(el);
+
+    if (electives.length > 0 && idx > -1) {
+      electives.splice(idx, 1); // remove selected elective
     }
   });
 
@@ -72,12 +83,21 @@ Template.diagramSidebar.helpers({
     let electives = getRemainingElectiveIds();
     let descriptions = [];
     electives.forEach(el => {
-      let description = Elective.findOne({id: el});
+      let description = Elective.findOne({id: el}).name;
       descriptions.push(description);
     });
 
     return descriptions;
   },
+
+  selected:function() {
+      let selectedElectiveIds = Meteor.user().profile.selectedElectives;
+      let electiveClasses = [];
+      selectedElectiveIds.forEach(elective => {
+         electiveClasses.push(Course.findOne({id:elective[1]}))
+      });
+      return electiveClasses;
+  }
 
   // TODO handle electives that have been specified but not completed
 });
