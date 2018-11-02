640f195999ea1dd0866fea04d1ac9f996ad8eef1
Marie <marie.yatsyk@gmail.com>
2018-11-01 20:28:41 -0400

    Sidebar logic (#10)
    * Pull in classes in sidebar
    
    * Add electives to sidebar
    
    * Add numCredits calculations
    
    * Refactored template sidebar helpers

---
+++ cdegree/client/diagram/events/sidebar.events.js
@@ -0,0 +1,8 @@
+Template.diagramSidebar.events({
+
+  'click .classes': function(event) {
+    $('#overlay').fadeIn();
+    Session.set('selectedCourse', event.currentTarget.dataset.id);
+  }
+
+});
---
+++ cdegree/client/diagram/helpers/template-sidebar.helpers.js
@@ -0,0 +1,83 @@
+function getDegree() {
+  let user = Meteor.user().profile;
+  return Degree.findOne({name: user.degree});
+}
+
+function getCompletedCourseIds() {
+  return Meteor.user().profile.completedReqCourses;
+}
+
+function getNumCompletedCredits() {
+  let userCompletedCourses = getCompletedCourseIds();
+  let acc = 0;
+  userCompletedCourses.forEach(course => {
+    acc += Course.findOne({id:course}).credits;
+  });
+  return acc;
+}
+
+function getRemainingElectiveIds() {
+  let user = Meteor.user().profile;
+  let electives = Degree.findOne({name: user.degree}).sections.electives.slice();
+  let userCompletedElectives = user.completedElectives.slice().map(
+      el => el[0]);
+
+  userCompletedElectives.forEach(el => {
+    // guard against student completing more electives than required
+    let idx = electives.indexOf(el);
+
+    if (electives.length > 0 && idx > -1) {
+      electives.splice(idx, 1); // remove completed elective
+    }
+  });
+
+  return electives;
+}
+
+
+Template.diagramSidebar.helpers({
+// returns whether the current course has been completed or not
+  degree: function () {
+    return getDegree();
+  },
+
+  completedCredits: function () {
+    return getNumCompletedCredits();
+  },
+
+  remainingCredits: function () {
+    let degree = getDegree();
+    return degree.semesterHoursRequired - getNumCompletedCredits() - getRemainingElectiveIds.length;
+  },
+
+  // returns incomplete courses
+  remaining: function () {
+    let degreeReqs = getDegree().sections.majorRequirements.slice();
+    let userCompletedClasses = getCompletedCourseIds();
+
+    let remaining = [];
+
+    degreeReqs.forEach((req) => {
+      if (!userCompletedClasses.includes(req)) {
+        let course = Course.findOne({id: req});
+        remaining.push(course);
+      }
+    });
+
+    return remaining;
+  },
+
+  // returns incomplete, unspecified electives
+  electives: function () {
+    let electives = getRemainingElectiveIds();
+    let descriptions = [];
+    electives.forEach(el => {
+      let description = Elective.findOne({id: el}).name;
+      descriptions.push(description);
+    });
+
+    return descriptions;
+  },
+
+  // TODO handle electives that have been specified but not completed
+});
