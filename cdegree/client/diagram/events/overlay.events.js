060a0ac84f6219d01fe902cede4933c8b3fe3962
Luciana Corteggiano <luciana.corteggiano@gmail.com>
2018-11-12 11:23:16 -0500

    choose button feedback

--- cdegree/client/diagram/events/overlay.events.js
+++ cdegree/client/diagram/events/overlay.events.js
@@ -1,17 +1,22 @@
 Template.overlayCourse.events({
+  "click .overlay-close": function(event) {
+    $("#overlay").fadeOut();
+  },
 
-    'click .overlay-close': function(event) {
-        $('#overlay').fadeOut();
-    },
-
-    'click .select-elective': function(event) {
-        let electiveType = parseInt(Session.get("selectedCourse"));
-        let profile = Meteor.user().profile;
-        let newElectives = profile.selectedElectives.slice();
-        newElectives.push([electiveType, event.currentTarget.dataset.id]);
-        profile['selectedElectives'] = newElectives;
-        Meteor.users.update({_id:Meteor.user()._id}, {$set: {profile: profile}});
-        $('#overlay').fadeOut();
-    }
-
+  "click .select-elective": function(event) {
+    let electiveType = parseInt(Session.get("selectedCourse"));
+    let profile = Meteor.user().profile;
+    let newElectives = profile.selectedElectives.slice();
+    newElectives.push([electiveType, event.currentTarget.dataset.id]);
+    profile["selectedElectives"] = newElectives;
+    Meteor.users.update(
+      { _id: Meteor.user()._id },
+      { $set: { profile: profile } }
+    );
+    $("#overlay").fadeOut();
+    $("#course-added")
+      .fadeIn(100)
+      .delay(1200)
+      .fadeOut(400);
+  }
 });
