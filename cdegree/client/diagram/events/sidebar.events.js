f3b027cbba10a375eefc59cb541bb0ae361dd34d
Luciana Corteggiano <luciana.corteggiano@gmail.com>
2018-11-12 11:28:33 -0500

    style fixes

--- cdegree/client/diagram/events/sidebar.events.js
+++ cdegree/client/diagram/events/sidebar.events.js
@@ -1,8 +1,6 @@
 Template.diagramSidebar.events({
-
-  'click .classes': function(event) {
-    $('#overlay').fadeIn();
-      Session.set('selectedCourse', event.currentTarget.dataset.id);
+  "click .classes": function(event) {
+    $("#overlay").fadeIn();
+    Session.set("selectedCourse", event.currentTarget.dataset.id);
   }
-
 });
