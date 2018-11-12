e4042aebb6819a302389b7a85bcfb48c0e7dd128
Zack Reiter <zack-r@users.noreply.github.com>
2018-11-12 13:21:27 -0500

    lots o stuff (#35)

--- cdegree/client/diagram/events/overlay.events.js
+++ cdegree/client/diagram/events/overlay.events.js
@@ -19,5 +19,40 @@ Template.overlayCourse.events({
       .fadeIn(100)
       .delay(1200)
       .fadeOut(400);
+  },
+  'click .replace-elective': function(event) {
+      let electiveId = Session.get("selectedCourse");
+      let selectedElectives = Meteor.user().profile.selectedElectives;
+      let electiveType = -1;
+      selectedElectives.forEach(elective => {
+          if (elective[1] === electiveId) {
+              electiveType = elective[0]
+          }
+      });
+      if (electiveType !== -1) {
+
+          Session.set("selectedCourse", electiveType.toString());
+          let foundIndex = selectedElectives.findIndex(elective => {
+              return elective[1] === electiveId;
+          });
+          selectedElectives.splice(foundIndex,1);
+          let toBeRemoved = [];
+          selectedElectives.forEach(elective => {
+             let course = Course.findOne({"id":elective[1]});
+             if (course.prereqs.includes(electiveId)) {
+                 toBeRemoved.push(elective);
+             }
+          });
+          toBeRemoved.forEach(el => {
+              let index = selectedElectives.indexOf(el);
+              selectedElectives.splice(index, 1);
+          });
+
+          let profile = Meteor.user().profile;
+          profile['selectedElectives'] = selectedElectives;
+          Meteor.users.update({_id:Meteor.user()._id}, {$set: {profile: profile}});
+      } else {
+          console.log("Error, elective type not found for selected elective.");
+      }
   }
 });
