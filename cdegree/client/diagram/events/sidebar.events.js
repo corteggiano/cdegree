b9ca454019de2959c7613c9e6efbc00ba8bb03ba
Luciana Corteggiano <luciana.corteggiano@gmail.com>
2018-11-12 11:51:28 -0500

    search classes in sidebar

--- cdegree/client/diagram/events/sidebar.events.js
+++ cdegree/client/diagram/events/sidebar.events.js
@@ -2,5 +2,21 @@ Template.diagramSidebar.events({
   "click .classes": function(event) {
     $("#overlay").fadeIn();
     Session.set("selectedCourse", event.currentTarget.dataset.id);
+  },
+  "keyup #search-class": function(event) {
+    let input, filter, d, i;
+    input = document.getElementById("search-class");
+    filter = input.value.toUpperCase();
+    classList = document.getElementById("classes-remaining");
+    d = classList.getElementsByTagName("div");
+    for (i = 0; i < d.length; i++) {
+      if (input === "") {
+        d[i].style.display = "";
+      } else if (d[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+        d[i].style.display = "";
+      } else {
+        d[i].style.display = "none";
+      }
+    }
   }
 });
