67e3b5b6c541d5d0fd6685b1a1dc1e0b70cbf0a6
maryats <maryats@users.noreply.github.com>
2018-11-15 14:35:49 -0500

    cytoscape is loading but not finding dagre layout

--- cdegree/client/diagram/events/diagram.events.js
+++ cdegree/client/diagram/events/diagram.events.js
@@ -1,19 +1,145 @@
+import cytoscape from 'cytoscape';
+
+let container = document.getElementById("cy");
+console.log("outside events", container);
+
 Template.diagram.events({
 
-    'click .course-node': function(event) {
-        $('#overlay').fadeIn();
+  'click .course-node': function (event) {
+    $('#overlay').fadeIn();
 
-        var course = Course.findOne({id: event.currentTarget.dataset.id});
+    var course = Course.findOne({id: event.currentTarget.dataset.id});
 
-        $('.prereq').removeClass("prereq");
+    $('.prereq').removeClass("prereq");
 
-        course.prereqs.forEach(function(id) {
+    course.prereqs.forEach(function (id) {
 
-            $('[data-id="'+id+'"]').addClass("prereq");
+      $('[data-id="' + id + '"]').addClass("prereq");
 
-        });
+    });
 
-        Session.set('selectedCourse', event.currentTarget.dataset.id);
-    }
+    Session.set('selectedCourse', event.currentTarget.dataset.id);
+  },
+});
 
+Template.diagram.onRendered(function () {
+
+  let container = document.getElementById("cy");
+  console.log(container);
+
+  data = {
+    container: container,
+
+    boxSelectionEnabled: false,
+    autounselectify: true,
+
+    layout: {
+      name: 'dagre'
+    },
+
+    style: [
+      {
+        selector: 'node',
+        style: {
+          'shape': 'roundrectangle',
+          'content': 'data(name)',
+          'height': 80,
+          'width': 150,
+          'text-opacity': 0.5,
+          'text-valign': 'center',
+          'text-halign': 'center',
+          'text-wrap': 'wrap',
+          'text-max-width': 130,
+          'background-color': '#efefef'
+        }
+      },
+
+      {
+        selector: "node[status = 'incomplete']",
+        style: {
+          'color': 'white',
+          'background-color': '#448bb2',
+        }
+      },
+      {
+        selector: "node[status = 'ip']",
+        style: {
+        }
+      },
+
+      {
+        selector: 'edge',
+        style: {
+          'curve-style': 'bezier',
+          'width': 4,
+          'target-arrow-shape': 'triangle',
+          'line-color': '#9dbaea',
+          'target-arrow-color': '#9dbaea'
+        }
+      }
+    ],
+
+    elements: {
+      nodes: [
+        {
+          data: {
+            id: 'CS 1800',
+            name: 'Discrete Structures',
+            status: 'complete'
+          }
+        },
+        {data: {id: 'CS 2800', name: 'Logic and Computation', status: 'ip'}},
+        {
+          data: {
+            id: 'CS 2500',
+            name: 'Fundamentals of Computer Science',
+            status: 'complete'
+          }
+        },
+        {
+          data: {
+            id: 'CS 2510',
+            name: 'Fundamentals of Computer Science 2',
+            status: 'complete'
+          }
+        },
+        {
+          data: {
+            id: 'CS 3500',
+            name: 'Object Oriented Design',
+            status: 'incomplete'
+          }
+        },
+        {data: {id: 'CS 3650', name: 'Computer Systems', status: 'ip'}},
+        {
+          data: {
+            id: 'CS 3700',
+            name: 'Networks and Distributed Systems',
+            status: 'incomplete'
+          }
+        },
+        {data: {id: 'CS 3800', name: 'Theory of Computation', status: 'ip'}},
+        {
+          data: {
+            id: 'CS 4400',
+            name: 'Programming Languages',
+            status: 'incomplete'
+          }
+        },
+      ],
+      edges: [
+        {data: {source: 'CS 1800', target: 'CS 2800'}},
+        {data: {source: 'CS 2500', target: 'CS 2510'}},
+        {data: {source: 'CS 2500', target: 'CS 3500'}},
+        {data: {source: 'CS 2500', target: 'CS 3650'}},
+        {data: {source: 'CS 2500', target: 'CS 3700'}},
+        {data: {source: 'CS 3500', target: 'CS 4400'}},
+        {data: {source: 'CS 3800', target: 'CS 4400'}},
+
+      ]
+    },
+  };
+
+  // cytoscapeData = data;
+  cytoscape(data);
 });
--- cdegree/client/diagram/helpers/diagram.helpers.js
+++ cdegree/client/diagram/helpers/diagram.helpers.js
@@ -1,120 +1,28 @@
-if(!Meteor.user()) {
-    Router.go("/");
+if (!Meteor.user()) {
+  Router.go("/");
 }
 
-import cytoscape from 'cytoscape';
-
-
-//cy.on('click', 'node', function(evt){
-  //  console.log( 'clicked ' + this.id() );
-//});
-
 Template.diagram.helpers({
+  //Returns all the courses for now TODO hook to user data
+  parents: function () {
 
-    cy:function() {
+    let degree = Degree.findOne({_id: Router.current().params._id});
+    let requirements = degree.sections.majorRequirements;
+    let selectedElectives = Meteor.user().profile.selectedElectives;
+    let selectedElectiveIds = selectedElectives.map(el => el[1]);
+    requirements = requirements.concat(selectedElectiveIds);
+    let parents = [];
 
-            data = {
-            container: document.getElementById('cy'),
+    requirements.forEach(function (element) {
+      let course = Course.findOne({id: element});
 
-            boxSelectionEnabled: false,
-            autounselectify: true,
+      if (course.prereqs.length === 0 && course.credits > 2) {
+        parents.push(course);
+      }
+    });
 
-            layout: {
-                name: 'dagre'
-            },
+    return parents;
 
-            style: [
-                {
-                    selector: 'node',
-                    style: {
-                        'shape': 'roundrectangle',
-                        'content': 'data(name)',
-                        'height': 80,
-                        'width': 150,
-                        'text-opacity': 0.5,
-                        'text-valign': 'center',
-                        'text-halign': 'center',
-                        'text-wrap': 'wrap',
-                        'text-max-width': 130,
-                        'background-color': '#efefef'
-                    }
-                },
-
-                {
-                    selector: "node[status = 'incomplete']",
-                    style: {
-                        'color': 'white',
-                        'background-color': '#448bb2',
-                    }
-                },
-                {
-                    selector: "node[status = 'ip']",
-                    style: {
-                        'background': 'repeating-linear-gradient(45deg,#e2eef5,#e2eef5 10px,#fff 0,#fff 20px);'
-                    }
-                },
-
-                {
-                    selector: 'edge',
-                    style: {
-                        'curve-style': 'bezier',
-                        'width': 4,
-                        'target-arrow-shape': 'triangle',
-                        'line-color': '#9dbaea',
-                        'target-arrow-color': '#9dbaea'
-                    }
-                }
-            ],
-
-            elements: {
-                nodes: [
-                    { data: { id: 'CS 1800', name: 'Discrete Structures', status: 'complete' } },
-                    { data: { id: 'CS 2800', name: 'Logic and Computation', status: 'ip' } },
-                    { data: { id: 'CS 2500', name: 'Fundamentals of Computer Science', status: 'complete' } },
-                    { data: { id: 'CS 2510', name: 'Fundamentals of Computer Science 2', status: 'complete' } },
-                    { data: { id: 'CS 3500', name: 'Object Oriented Design', status: 'incomplete' } },
-                    { data: { id: 'CS 3650', name: 'Computer Systems', status: 'ip' } },
-                    { data: { id: 'CS 3700', name: 'Networks and Distributed Systems', status: 'incomplete' } },
-                    { data: { id: 'CS 3800', name: 'Theory of Computation', status: 'ip' } },
-                    { data: { id: 'CS 4400', name: 'Programming Languages', status: 'incomplete'} },
-                ],
-                edges: [
-                    { data: { source: 'CS 1800', target: 'CS 2800' } },
-                    { data: { source: 'CS 2500', target: 'CS 2510' } },
-                    { data: { source: 'CS 2500', target: 'CS 3500' } },
-                    { data: { source: 'CS 2500', target: 'CS 3650' } },
-                    { data: { source: 'CS 2500', target: 'CS 3700' } },
-                    { data: { source: 'CS 3500', target: 'CS 4400' } },
-                    { data: { source: 'CS 3800', target: 'CS 4400' } },
-
-                ]
-            },
-        };
-
-        return data;
-
-    },
-
-    //Returns all the courses for now TODO hook to user data
-    parents:function(){
-
-        let degree = Degree.findOne({_id: Router.current().params._id});
-        let requirements = degree.sections.majorRequirements;
-        let selectedElectives = Meteor.user().profile.selectedElectives;
-        let selectedElectiveIds = selectedElectives.map(el => el[1]);
-        requirements = requirements.concat(selectedElectiveIds);
-        let parents = [];
-
-        requirements.forEach(function(element) {
-            let course = Course.findOne({id: element});
-
-            if(course.prereqs.length === 0 && course.credits > 2) {
-                parents.push(course);
-            }
-        });
-
-        return parents;
-
-    }
+  }
 
 });
